ef0f5c6c0c5017dc19dd53cebef3c74a265dc835
manh <manh@balanceinternet.com.au>
2018-04-06 16:59:47 +0700

    update

--- wp/index.php
+++ wp/index.php
@@ -15,3 +15,51 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Retrieves or sets the Symfony Dependency Injection container
+ *
+ * @param ContainerInterface|string $id
+ *
+ * @return mixed
+ */
+function symfony($id)
+{
+    static $container;
+
+    if ($id instanceof ContainerInterface) {
+        $container = $id;
+        return;
+    }
+
+    return $container->get($id);
+}
+
+$loader = require __DIR__.'/../sf/vendor/autoload.php';
+require_once __DIR__.'/../sf/var/bootstrap.php.cache';
+
+// Load application kernel
+require_once __DIR__.'/../sf/app/AppKernel.php';
+
+$sfKernel = new AppKernel('dev', true);
+$sfKernel->loadClassCache();
+$sfKernel->boot();
+
+// Add Symfony container as a global variable to be used in Wordpress
+$sfContainer = $sfKernel->getContainer();
+
+if (true === $sfContainer->getParameter('kernel.debug', false)) {
+    Debug::enable();
+}
+
+symfony($sfContainer);
+
+$sfRequest = Request::createFromGlobals();
+$sfResponse = $sfKernel->handle($sfRequest);
+$sfResponse->send();
+
+$sfKernel->terminate($sfRequest, $sfResponse);
