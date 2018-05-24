d7d57b91a13e57c836c0977f20ac62e995fec5e4
manh-nguyen <mymanhapt@gmail.com>
2018-05-24 22:38:43 +0700

    Configure streaming url

--- sf/src/AppBundle/Form/AssetType.php
+++ sf/src/AppBundle/Form/AssetType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,6 @@ class AssetType extends AbstractType
             'data' => strtoupper(hash('adler32', uniqid(rand(), true))),
         ])
             ->add('state', HiddenType::class, ['data' => 1])
-            ->add('name', HiddenType::class, ['data' => 'name'])
             ->add('title')
             ->add('description', CKEditorType::class, array(
                 'config' => array(
@@ -28,7 +28,8 @@ class AssetType extends AbstractType
             ))
             ->add('options', HiddenType::class, ['data' => 1])
             ->add('formatOption', HiddenType::class, ['data' => 1])
-            ->add('uri', HiddenType::class)
+            ->add('uri', null, ['data' => 'http://ydimedia-aase.streaming.media.azure.net/12dfb987-3ea1-44a3-8216-f1837fd571ab/'])
+            ->add('name', null, ['data' => 'Wildlife'])
             ->add('storageAccountName', HiddenType::class)
             ->add('alternateId', HiddenType::class);
     }
