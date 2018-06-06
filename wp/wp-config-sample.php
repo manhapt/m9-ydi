5a99196c706842bbfaaded86cbcc91cb201b72da
manh <manh@balanceinternet.com.au>
2018-06-06 14:11:21 +0700

    use the MySQL Connection String from Azure

--- wp/wp-config-sample.php
+++ wp/wp-config-sample.php
@@ -20,16 +20,34 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+$connectStr_dbHost = 'localhost';
+$connectStr_dbName = 'database_name_here';
+$connectStr_dbUsername = 'username_here';
+$connectStr_dbPassword = 'password_here';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+
+    $connectStr_dbHost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectStr_dbName = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectStr_dbUsername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectStr_dbPassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', $connectStr_dbName);
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', $connectStr_dbUsername);
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', $connectStr_dbPassword);
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', $connectStr_dbHost);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
